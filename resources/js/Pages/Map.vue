88425e462663fc6cd4f2ece7e552f4061bc5d661
DrozdAllan <drozdallan@gmail.com>
2022-07-21 23:57:21 +0200

    bon deja

--- resources/js/Pages/Map.vue
+++ resources/js/Pages/Map.vue
@@ -5,9 +5,9 @@
 				<h3>
 					 China Map</h3>
 		  </el-row>
-		  https://www.npmjs.com/package/leaflet-china-map-provider
+		  <p>faire une liste déroulante ou tu peux choisir d'encercler des grandes villes, les rivières... voire plus</p>
 		  <el-row justify="center" style="padding: 10px 0 10px 0">
-				<l-map style="height: 350px" :zoom="zoom" :center="center">
+				<l-map style="height: 500px" :zoom="zoom" :center="center" >
 					 <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
 					 <l-circle :lat-lng="circle.center" :radius="circle.radius" :color="circle.color" />
 				</l-map>
@@ -24,10 +24,10 @@ import {LMap, LTileLayer, LCircle} from "@vue-leaflet/vue-leaflet";
 const mapIsReady = ref(false);
 const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attribution = '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-const zoom = 8;
-const center = [47.313220, -1.319482];
+const zoom = 12;
+const center = [31.59, 120.29];
 const circle = {
-    center: [47.413220, -1.0482],
+    center: [31.59, 120.29],
     radius: 4500,
     color: 'red'
 };
