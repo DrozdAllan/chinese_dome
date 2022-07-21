a647ac626065ab06f40101e9998efeeff3df3e2b
DrozdAllan <drozdallan@gmail.com>
2022-07-21 18:39:14 +0200

    added vue-leaflet, TODO: add a package or change tiles layer

--- resources/js/Pages/Map.vue
+++ resources/js/Pages/Map.vue
@@ -3,16 +3,32 @@
 	 <Layout>
 		  <el-row justify="center" align="middle">
 				<h3>
-					 Interactive Map, ptet Vue Leaflet et tout faire soi même mdr</h3>
+					 China Map</h3>
 		  </el-row>
+		  https://www.npmjs.com/package/leaflet-china-map-provider
 		  <el-row justify="center" style="padding: 10px 0 10px 0">
-				<h2>Wo xiang xing ni jia da</h2> <br> <span> OpenWeatherMap or WeatherApi</span>
+				<l-map style="height: 350px" :zoom="zoom" :center="center">
+					 <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+					 <l-circle :lat-lng="circle.center" :radius="circle.radius" :color="circle.color" />
+				</l-map>
 		  </el-row>
 	 </Layout>
 </template>
-<script setup lang="ts">
-import Layout from './Layout.vue'
-import {Head} from "@inertiajs/inertia-vue3";
-import {ref} from 'vue'
+<script setup>
+import Layout from './Layout.vue';
+import {Head} from '@inertiajs/inertia-vue3';
+import {onBeforeMount, ref} from "vue";
+import 'leaflet/dist/leaflet.css';
+import {LMap, LTileLayer, LCircle} from "@vue-leaflet/vue-leaflet";
+
+const mapIsReady = ref(false);
+const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution = '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+const zoom = 8;
+const center = [47.313220, -1.319482];
+const circle = {
+    center: [47.413220, -1.0482],
+    radius: 4500,
+    color: 'red'
+};
 </script>
-<style scoped></style>
